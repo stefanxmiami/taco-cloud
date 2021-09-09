42b26372fd8c1d85f6728389a9d0047c06e1b445
Stefan Ivanov <stefan.miami99@gmail.com>
2021-09-09 13:05:56 +0200

    Basic features

---
+++ src/main/java/tacos/Order.java
@@ -0,0 +1,27 @@
+package tacos;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class Order {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "Street is required")
+    private String street;
+    @NotBlank(message = "City is required")
+    private String city;
+    @CreditCardNumber(message = "Not a valid Credit Card number")
+    private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message = "Must be formatted MM/YY")
+    private String ccExpiration;
+    @Digits(integer = 3,fraction = 0,message = "Invalid CVV")
+    private String ccCVV;
+}
